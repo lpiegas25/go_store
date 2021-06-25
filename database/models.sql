6d4a84a7363fbba9dec3e78c737ef66e07458f8f
Lucas Piegas <lucaspiegas25@gmail.com>
2021-06-25 00:27:46 -0300

    model item change MER-UML actualizado

--- database/models.sql
+++ database/models.sql
@@ -102,18 +102,6 @@ CREATE TABLE IF NOT EXISTS warehouses (
     unique (name)
 );
 
-CREATE TABLE IF NOT EXISTS items (
-    id serial NOT NULL,
-    warehouse_id int not null,
-    delivery_id int,
-    name text NOT NULL,
-    description text NOT NULL,
-    price numeric(18,3) NOT NULL,
-    created_at timestamp DEFAULT now(),
-    updated_at timestamp NOT NULL,
-    CONSTRAINT pk_items PRIMARY KEY(id)
-);
-
 create table if not exists deliveries_employees (
     id serial not null,
     delivery_id int not null,
@@ -122,6 +110,17 @@ create table if not exists deliveries_employees (
     updated_at timestamp NOT NULL,
     CONSTRAINT pk_deliveries_employees PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS items (
+    id serial NOT NULL,
+    warehouse_id int,
+    name text NOT NULL,
+    description text NOT NULL,
+    price numeric(18,3) NOT NULL,
+    created_at timestamp DEFAULT now(),
+    updated_at timestamp NOT NULL,
+    CONSTRAINT pk_items PRIMARY KEY(id)
+    );
 
 create table if not exists items_deliveries (
     id serial not null,
@@ -144,9 +143,9 @@ create table if not exists items_warehouses (
 );
 
 alter table items drop constraint if exists fk_items_warehouses;
-alter table items drop constraint if exists fk_items_deliveries;
+--alter table items drop constraint if exists fk_items_deliveries;
 alter table items add CONSTRAINT fk_items_warehouses FOREIGN KEY(warehouse_id) REFERENCES warehouses(id);
-alter table items add CONSTRAINT fk_items_deliveries FOREIGN KEY(delivery_id) REFERENCES deliveries(id);
+--alter table items add CONSTRAINT fk_items_deliveries FOREIGN KEY(delivery_id) REFERENCES deliveries(id);
 
 alter table employees drop constraint if exists fk_employees_roles;
 alter table employees add constraint fk_employees_roles foreign key(role_id) references roles(id);
